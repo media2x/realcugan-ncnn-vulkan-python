83b52aecc3908e49b3bee17050afe39f1500bd8a
k4yt3x <i@k4yt3x.com>
2022-03-19 16:30:08 +0000

    added basic metadata and CI files

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import pathlib
+
+import cmake_build_extension
+import setuptools
+
+cmake_flags = [
+    "-DBUILD_SHARED_LIBS:BOOL=OFF",
+    "-DCALL_FROM_SETUP_PY:BOOL=ON",
+]
+cmake_flags.extend(os.environ.get("CMAKE_FLAGS", "").split())
+
+setuptools.setup(
+    ext_modules=[
+        cmake_build_extension.CMakeExtension(
+            name="realcugan-ncnn-vulkan-python",
+            install_prefix="realcugan_ncnn_vulkan_python",
+            write_top_level_init="from .realcugan_ncnn_vulkan import Realcugan, RealCUGAN",
+            source_dir=str(
+                pathlib.Path(__file__).parent / "realcugan_ncnn_vulkan_python"
+            ),
+            cmake_configure_options=cmake_flags,
+        )
+    ],
+    cmdclass={"build_ext": cmake_build_extension.BuildExtension},
+)
